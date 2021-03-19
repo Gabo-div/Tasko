b3ab1d4d8e6f30152e175b4e64d9e28095d37320
Gabo-div <64453625+Gabo-div@users.noreply.github.com>
2021-03-19 15:45:44 -0400

    Update pwa.js

--- pwa.js
+++ pwa.js
@@ -12,57 +12,49 @@ const Cache_Name = 'v1_cache_tasko',
     ];
 
 //durante la fase de instalación, generalmente se almacena en caché los activos estáticos
-self.addEventListener('install', e =>{
-
-    e.waitUntil(
-        caches.open(Cache_Name)
-          .then(cache => {
-            return cache.addAll(urlsToCache)
-              .then(() => self.skipWaiting())
-          })
-          .catch(err => console.log('Falló registro de cache', err))
-      )
-
-    
-
-});
-
-//Se activa y guarda los recursos en cache para funcionar sin internet
-self.addEventListener('activate', e =>{
-
-    const cacheWhitelist = [Cache_Name];
-
-    e.waitUntil(
-        caches.keys()
-          .then(cacheNames => {
-            return Promise.all(
-              cacheNames.map(cacheName => {
-                //Eliminamos lo que ya no se necesita en cache
-                if (cacheWhitelist.indexOf(cacheName) === -1) {
-                  return caches.delete(cacheName)
-                }
-              })
-            )
-          })
-          // Le indica al SW activar el cache actual
-          .then(() => self.clients.claim())
-      )
-
-});
-
-//Cuando el navegador recupera una url
-self.addEventListener('fetch', e =>{
-
-    e.respondWith(
-        caches.match(e.request)
-          .then(res => {
-            if (res) {
-              //recuperar del cache
-              return res
-            }
-            //recuperar de la petición a la url
-            return fetch(e.request)
-          })
-      )
-
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(Cache_Name)
+      .then(cache => {
+        return cache.addAll(urlsToCache)
+          .then(() => self.skipWaiting())
+      })
+      .catch(err => console.log('Falló registro de cache', err))
+  )
 })
+
+//una vez que se instala el SW, se activa y busca los recursos para hacer que funcione sin conexión
+self.addEventListener('activate', e => {
+  const cacheWhitelist = [Cache_Name]
+
+  e.waitUntil(
+    caches.keys()
+      .then(cacheNames => {
+        return Promise.all(
+          cacheNames.map(cacheName => {
+            //Eliminamos lo que ya no se necesita en cache
+            if (cacheWhitelist.indexOf(cacheName) === -1) {
+              return caches.delete(cacheName)
+            }
+          })
+        )
+      })
+      // Le indica al SW activar el cache actual
+      .then(() => self.clients.claim())
+  )
+})
+
+//cuando el navegador recupera una url
+self.addEventListener('fetch', e => {
+  //Responder ya sea con el objeto en caché o continuar y buscar la url real
+  e.respondWith(
+    caches.match(e.request)
+      .then(res => {
+        if (res) {
+          //recuperar del cache
+          return res
+        }
+        //recuperar de la petición a la url
+        return fetch(e.request)
+      })
+  )
